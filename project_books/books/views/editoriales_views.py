8f4d30fcecd102e6f90b8f47d977b9caa0e40097
jesus <jesuslopj0@gmail.com>
2024-11-10 20:52:46 +0100

    Actualizando views y templates Editorial

--- project_books/books/views/editoriales_views.py
+++ project_books/books/views/editoriales_views.py
@@ -1,9 +1,11 @@
  
+from typing import Any
 from django.shortcuts import render
 from books.models import Editorial
 from project_books.forms import SearchForm
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 editoriales_objects = Editorial.objects.all()
 
@@ -32,24 +34,41 @@ def editoriales_view(request):
 class EditorialesListView(ListView):
     model = Editorial
     template_name = 'editorial/EditorialesList.html'
+    context_object_name = 'context'
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["search_form"] = SearchForm()
+        return context
 
 
 class EditorialDetailView(DetailView):
     model = Editorial
     template_name = 'editorial/EditorialDetail.html'
-
+    context_object_name = 'editorial'
+    pk_url_kwarg = 'id'
 
 
 class EditorialCreateView(CreateView):
     model = Editorial
     template_name = 'editorial/EditorialCreate.html'
+    fields = ['nombre', 'direccion', 'ciudad', 'pais']
 
+    def get_success_url(self):
+        return reverse_lazy('editoriales:detail', kwargs={'id': self.object.id})
 
 
 class EditorialUpdateView(UpdateView):
     model = Editorial
     template_name = 'editorial/EditorialUpdate.html'
+    pk_url_kwarg = 'id'
+
+    def get_success_url(self):
+        return reverse_lazy('editoriales:detail', kwargs={'id': self.object.id})
 
 
 class EditorialDeleteView(DeleteView):
     model = Editorial
+    template_name = 'editorial/EditorialDelete.html'
+    success_url = reverse_lazy('editoriales:list') 
+    pk_url_kwarg = 'id'
